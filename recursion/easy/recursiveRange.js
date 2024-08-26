5e59e002d14b094319d8be4a29b8e5901855dd54
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2024-08-26 16:26:53 +0530

    Create recursiveRange.js

---
+++ recursion/easy/recursiveRange.js
@@ -0,0 +1,18 @@
+// recursiveRange
+
+/*
+Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function
+*/
+
+function recursiveRange(num) {
+  if (num === 0) {
+    return 0;
+  }
+
+  return num + recursiveRange(num - 1);
+}
+
+// TESTING
+
+console.log(recursiveRange(5)); // 15
+console.log(recursiveRange(6)); // 15
