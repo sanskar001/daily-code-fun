20bf15105ee03a3712570503062c7b2054980112
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2024-12-05 13:18:36 +0530

    Create removeDuplicates.js

---
+++ practice/arrays/removeDuplicates.js
@@ -0,0 +1,53 @@
+/*
+26. Remove Duplicates from Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums. Return k.
+ 
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+*/
+
+var removeDuplicates = function (nums) {
+  let i = 0;
+  let j = 1;
+  const size = nums.length;
+
+  while (j < size) {
+    if (nums[j] !== nums[i]) {
+      i++;
+      nums[i] = nums[j];
+    }
+    j++;
+  }
+
+  // Remove all rest of items after i
+
+  for (let k = i + 1; k < size; k++) {
+    nums.pop();
+  }
+
+  console.log(nums);
+
+  return i + 1;
+};
+
+// TESTING
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
