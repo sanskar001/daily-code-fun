8586397e3c72557bd6f01f895b5a538766bbd264
Sanskar Maheshwari <46653101+sanskar001@users.noreply.github.com>
2024-12-25 22:13:19 +0530

    Create removeOuterParentheses.js

---
+++ string/easy/removeOuterParentheses.js
@@ -0,0 +1,67 @@
+/*
+1021. Remove Outermost Parentheses
+
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+*/
+
+// SOLUTION --- TIME COMPLEXITY => O(N) & AUX SPACE COMPLEXITY => O(N)
+
+var removeOuterParentheses = function (s) {
+  let i = 0;
+  let ans = "";
+  let open = 0;
+
+  while (i < s.length) {
+    if (s[i] === "(") {
+      if (open > 0) {
+        ans += "(";
+      }
+      open++;
+    } else {
+      open--;
+      if (open > 0) {
+        ans += ")";
+      }
+    }
+    i++;
+  }
+
+  return ans;
+};
+
+// TESTING
+
+console.log(removeOuterParentheses("(()())"));
+console.log(removeOuterParentheses("(()())(())"));
+console.log(removeOuterParentheses("(()())(())(()(()))"));
+console.log(removeOuterParentheses("()()"));
